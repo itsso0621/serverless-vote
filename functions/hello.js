c37bd647130d0bd821a433747e3482bca22032a3
SoYoungLee <59616442+itsso0621@users.noreply.github.com>
2020-10-18 19:09:03 -0400

    Add files via upload

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,23 @@
 // functions/hello.js
+let votesArray = [0, 0, 0, 0];
+
 exports.handler = async (event) => {
-  const subject = event.queryStringParameters.name || "World";
+  const v = event.queryStringParameters.vote;
+
+  if (v === "a") {
+    votesArray[0] += 1;
+  } else if (v === "b") {
+    votesArray[1] += 1;
+  } else if (v === "b") {
+    votesArray[2] += 1;
+  } else if (v === "b") {
+    votesArray[3] += 1;
+  }
+  console.log(JSON.stringify(votesArray));
+
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`,
+    header: { "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(votesArray),
   };
 };
