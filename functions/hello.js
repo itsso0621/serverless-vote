1816b332e20838d27fb23277ccb2ab8cc8c6a2be
SoYoungLee <59616442+itsso0621@users.noreply.github.com>
2020-10-18 18:55:29 -0400

    again

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,8 @@
+// functions/hello.js
 exports.handler = async (event) => {
   const subject = event.queryStringParameters.name || "World";
   return {
     statusCode: 200,
-    bodt: `Hello ${subject}`,
+    body: `Hello ${subject}!`,
   };
 };
